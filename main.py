d988b3a99a9cbbea052c6857423a2b80d19733d2
Antonchek <aboba@gmail.com>
2024-08-07 02:53:20 +0300

    Refactore echo_message function

--- main.py
+++ main.py
@@ -26,14 +26,15 @@ def send_random_joke(message: types.Message):
 def echo_message(message: types.Message):
     """Communicate with user. With unknown command or message send echo-message
     """
-    if 'привет' in message.text.lower():
-        bot.send_message(message.chat.id, 'И тебе привет!')
-    elif 'как дела?' in message.text.lower():
-        bot.send_message(message.chat.id, 'Хорошо! А у Вас как?')
-    elif any(word in message.text.lower() for word in ['пока', 'до свидания']): # fast-cycle for any word from list in message
-        bot.send_message(message.chat.id, 'До новых встреч!')
-    else:
-        bot.send_message(message.chat.id, message.text)
+    text = message.text
+    text_lower = text.lower()
+    if 'привет' in text_lower:
+        text = 'И тебе привет!'
+    elif 'как дела' in text_lower:
+        text = 'Хорошо! А у Вас как?'
+    elif any(word in text_lower for word in ['пока', 'до свидания']): # fast-cycle for any word from list in message
+        text = 'До новых встреч!'
+    bot.send_message(message.chat.id, text)
 
 bot.infinity_polling(skip_pending=True) # Infinity cycle for handling users messages, 
                                         # skip_pending - ignore "waiting" messages (disable before global launch)
